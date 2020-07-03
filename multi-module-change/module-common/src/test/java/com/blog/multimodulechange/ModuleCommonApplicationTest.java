78dee63bccb395c03ca13018300358258caacb1d
ordinCode <ysh1346@naver.com>
2020-07-03 16:56:51 +0900

    module-common 분리 테스트 성공

---
+++ multi-module-change/module-common/src/test/java/com/blog/multimodulechange/ModuleCommonApplicationTest.java
@@ -0,0 +1,10 @@
+package com.blog.multimodulechange;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleCommonApplicationTest {
+    public void contextLoads() {
+
+    }
+}
---
+++ multi-module-change/module-common/src/test/java/com/blog/multimodulechange/domain/UserRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.blog.multimodulechange.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void save() {
+        User user = User.builder()
+                .name("홍길동")
+                .password("1234")
+                .build();
+
+        User savedUser = userRepository.save(user);
+
+        assertThat(savedUser.getId()).isNotNull();
+    }
+}
