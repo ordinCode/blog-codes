95b1c9fd33489c0ae40c10ae6ba13d657cf2923f
ordinCode <ysh1346@naver.com>
2020-07-03 12:22:03 +0900

    init

---
+++ multi-module-change/src/main/java/com/blog/multimodulechange/MultiModuleChangeApplication.java
@@ -0,0 +1,13 @@
+package com.blog.multimodulechange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleChangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleChangeApplication.class, args);
+    }
+
+}
---
+++ multi-module-change/src/test/java/com/blog/multimodulechange/MultiModuleChangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.multimodulechange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleChangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
