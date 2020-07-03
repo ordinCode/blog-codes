33e1040dff9be46844397886788489ba8a30c0b2
ordinCode <ysh1346@naver.com>
2020-07-03 17:04:01 +0900

    module-api 분리 테스트 성공

--- multi-module-change/src/main/java/com/blog/multimodulechange/controller/Controller.java
+++ multi-module-change/src/main/java/com/blog/multimodulechange/controller/Controller.java
@@ -1,17 +1,11 @@
 package com.blog.multimodulechange.controller;
 
 import com.blog.multimodulechange.domain.User;
-import com.blog.multimodulechange.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class UserController {
-    private final UserService userService;
-
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
+public class Controller {
 
     @GetMapping("/")
     public User getTest(){
