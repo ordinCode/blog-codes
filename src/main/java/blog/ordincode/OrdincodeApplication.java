213909188e96d99765a2efbc6605daa267f52652
ordinCode <ysh1346@naver.com>
2020-08-13 21:39:46 +0900

    init

---
+++ src/main/java/blog/ordincode/OrdincodeApplication.java
@@ -0,0 +1,13 @@
+package blog.ordincode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdincodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdincodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/blog/ordincode/OrdincodeApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.ordincode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdincodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
