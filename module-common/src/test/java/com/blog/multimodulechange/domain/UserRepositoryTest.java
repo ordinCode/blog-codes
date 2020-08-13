db91c6f26fc8eb4587eaef94bff44b20414324cc
ordinCode <ysh1346@naver.com>
2020-08-13 21:55:27 +0900

    init

--- module-common/src/test/java/com/blog/multimodulechange/domain/UserRepositoryTest.java
+++ module-common/src/test/java/com/blog/multimodulechange/domain/UserRepositoryTest.java
@@ -4,8 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class UserRepositoryTest {
